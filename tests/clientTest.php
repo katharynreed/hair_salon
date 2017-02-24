df0c12ae88c55938077c78a02795288c0a5cc3a6
Katie Reed <katharynreedpierce@gmail.com>
2017-02-24 09:22:07 -0800

    create index.html and json

--- tests/clientTest.php
+++ tests/clientTest.php
@@ -1,3 +1,16 @@
 <?php
 
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+require_once 'src/Client.php';
+
+$server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
+
 ?>
--- tests/stylistTest.php
+++ tests/stylistTest.php
@@ -1,3 +1,15 @@
 <?php
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+require_once 'src/Stylist.php';
+
+$server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
 
 ?>
