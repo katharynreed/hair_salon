d724a9f419dc08fd316c5b942e3ea6eacbed5edb
Katie Reed <katharynreedpierce@gmail.com>
2017-02-24 13:52:48 -0800

    add passing test condition for delete and getAll stylist class

--- src/stylist.php
+++ src/stylist.php
@@ -27,6 +27,16 @@
             $this->name = $new_name;
         }
 
+        function getBio()
+        {
+            return $this->bio;
+        }
+
+        function setBio()
+        {
+            $this->bio = $new_bio;
+        }
+
         function getClientID()
         {
             return $this->client_id;
@@ -39,8 +49,15 @@
 
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO stylists (name, bio, client_id) VALUES ('{$this->getName()}', {$this->getClientId()});");
+            // print_r("\nObject to save:\n");
+            // print_r("{$this->getName()}\n");
+            // print_r("{$this->getBio()}\n");
+            // print_r("{$this->getClientID()}\n");
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name, bio, client_id) VALUES ('{$this->getName()}', '{$this->getBio()}', {$this->getClientID()});");
             $this->id = $GLOBALS['DB']->lastInsertId();
+            // print_r("\nTHIS IS THE SAVE FUNCTION\n\n");
+            // print_r($this->id);
+            // print_r("\nEND OF SAVE FUNCTION\n\n");
         }
 
         function find($search_id)
@@ -62,24 +79,34 @@
             $this->setName($new_name);
         }
 
-        static function deleteAll()
+        function delete()
         {
-            $GLOBALS['DB']->exec('DELETE FROM stylists');
+            $GLOBALS['DB']->exec("DELETE FROM stylists WHERE id = {$this->getId()};");
         }
+
 
         static function getAll()
         {
-            $returned_stylists = $GLOBALS['DB']->query('SELECT * FROM stylists');
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            // print_r("Returned stylists\n");
+            // print_r($resturned_stylists);
             $stylists = [];
-            foreach ($returned_stylists as $stylist) {
+            foreach($returned_stylists as $stylist) {
                 $name = $stylist['name'];
                 $bio = $stylist['bio'];
-                $client_id = $stylist['id'];
+                $client_id = $stylist['client_id'];
+                $id = $stylist['id'];
                 $new_stylist = new Stylist($name, $bio, $client_id, $id);
                 array_push($stylists, $new_stylist);
             }
             return $stylists;
         }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec('DELETE FROM stylists');
+        }
+
     }
 
 ?>
--- tests/stylistTest.php
+++ tests/stylistTest.php
@@ -31,6 +31,8 @@
             $client_id = $test_Client->getId();
             $test_stylist = new Stylist ($name, $bio, $client_id);
             $test_stylist->save();
+            //get above stylist FROM database
+            //compare result from DB to 'Sally Style'
 
             $result = $test_stylist->getName();
 
@@ -68,7 +70,7 @@
 
             $result = Stylist::find($test_stylist->getId());
 
-            $this->assertEquals($test_Stylist, $result);
+            $this->assertEquals($test_stylist, $result);
         }
 
         function test_update()
@@ -88,6 +90,52 @@
             $test_stylist->update($new_name);
 
             $this->assertEquals('Sally Field-Style', $test_stylist->getName());
+        }
+
+        function test_delete()
+        {
+            $client_name = 'Jane Doe';
+            $test_Client = new Client ($client_name);
+            $test_Client->save();
+            $client_id = $test_Client->getId();
+
+            $name = 'Sally Style';
+            $bio = 'Sally does a lot of hair.';
+            $test_stylist = new Stylist ($name, $bio, $client_id);
+            $test_stylist->save();
+
+            $name2 = 'Sam Style';
+            $bio2 = 'Sam does a lot of hair.';
+            $test_stylist2 = new Stylist ($name2, $bio2, $client_id);
+            $test_stylist2->save();
+
+            $test_stylist2->delete();
+
+            $this->assertEquals([$test_stylist], Stylist::getAll());
+        }
+
+        function test_getAll()
+        {
+            $client_name = 'Jane Doe';
+            $test_Client = new Client($client_name);
+            $test_Client->save();
+            $client_id = $test_Client->getId();
+
+            $name = 'Sally Style';
+            $bio = 'Sally does a lot of hair.';
+            $test_stylist = new Stylist ($name, $bio, $client_id);
+            $test_stylist->save();
+
+            $name2 = 'Sam Style';
+            $bio2 = 'Sam does a lot of hair.';
+            $test_stylist2 = new Stylist ($name2, $bio2, $client_id);
+            $test_stylist2->save();
+
+            $result = Stylist::getAll();
+            // print_r("Here is the result!!\n\n");
+            // print_r($result);
+
+            $this->assertEquals([$test_stylist, $test_stylist2], $result);
         }
     }
 
