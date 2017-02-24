bb357bce8a4ef7e96742ca29ce8296e1f4b18294
Katie Reed <katharynreedpierce@gmail.com>
2017-02-24 15:03:41 -0800

    add root.html

--- app/app.php
+++ app/app.php
@@ -1,3 +1,24 @@
 <?php
+    date_default_timezone_set('America/Los_Angeles');
 
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__.'/../src/stylist.php';
+    require_once __DIR__.'/../src/client.php';
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function () use ($app) {
+        return $app['twig']->render('root.html.twig');
+    });
+
+    return $app;
 ?>
